81525049304c5b4957705518a4c71954318a5565
Camille Condrey <camillecondrey@gmail.com>
2017-03-06 19:01:54 -0700

    final commit

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ $(document).ready(function () {
 
 var youtube_base_url = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromApi(searchTerm) {	
+var nextPageToken; 
+
+var prevPageToken; 
+
+function getDataFromApi(searchTerm, token) {	
 	var query = 
 	{
 		'part': 'snippet',
 		'key': 'AIzaSyDI0Hu0knTBdaZF6IbrJPKEq-Fk-2mf2lQ',
-		'q': searchTerm 
+		'q': searchTerm, 
+		'pageToken': token
 	};
 	$.getJSON(youtube_base_url, query, function (data){	
+			nextPageToken = data.nextPageToken
+			prevPageToken = data.prevPageToken
+
 			if (data.pageInfo.totalResults > 0){
 				displayYoutubeSearchData(data.items);
 		}
@@ -25,11 +33,15 @@ function displayYoutubeSearchData(items) {
 	var html = "";
 	$.each(items, function (index, item){
 	// var html = '';	
-		console.log(item.snippet.title);
+		console.log(item);
 		console.log(item.snippet.thumbnails.medium.url);
-		html += '<p class="item">' + item.snippet.title + 
-		"</p><img src='" + item.snippet.thumbnails.medium.url 
-		+ "'/>";
+		// html += '<p class="item">' + item.snippet.title + 
+		// '</p><a href="https://www.youtube.com/watch?v=">' + ' <img src='" + item.snippet.thumbnails.medium.url 
+		// + "'/></a>";
+		html += `<div class="result-container"<p class="item">${item.snippet.title}</p>
+		<a href="https://www.youtube.com/watch?v=${item.id.videoId}">
+		<img src="${item.snippet.thumbnails.medium.url}"/></a></div>` 
+		// console.log(x);
 	});
 	
 
@@ -37,31 +49,24 @@ function displayYoutubeSearchData(items) {
 	$('.js-search-results').html(html);
 };
 
+$('#Next').click(function(event){
+	getDataFromApi($('.js-query').val(), nextPageToken);
+});
+
+$("#Previous").click(function(event){
+	getDataFromApi($('.js-query').val(), prevPageToken);
+});
+
+$('#submit').click(function(event){
+	$('#Next').removeClass('hidden');
+	$('#Previous').removeClass('hidden');
+});
+
 
 $('.js-search-form').submit(function(event){
 	event.preventDefault();
-	getDataFromApi($('.js-query').val());
+	getDataFromApi($('.js-query').val(), null);
 });
 });
 
 
-// 	if (data.Search) {
-// 		data.Search.forEach(function(item){
-// 			resultElement += '<p>' + item.Title + '</p>';
-// 		});
-// 	}
-// 	else {
-// 		resultElement += '<p>No results</p>';
-// 	}
-// 	$('.js-search-results').html(resultElement);
-// }
-
-// function watchSubmit() {
-// 	$('js-search-form').submit(function(event){
-// 		event.preventDefault();
-// 		var query = $(this).find('.js-query').val();
-// 		getDataFromApi(query, displayYoutubeSearchData);
-// 	});
-// }
-
-// $(function(){watchSubmit();});
